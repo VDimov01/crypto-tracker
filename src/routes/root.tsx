4d5289ddc97d190521e3d1aaab81f1e477f2f046
VDimov01 <vdimov54@gmail.com>
2023-07-19 21:56:38 +0300

    add search functionality
    search field added and functionality implemented

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -3,6 +3,7 @@ import { Crypto } from "../services/model";
 import { Header } from "../components/Header/Header";
 import { Home } from "../components/Home/Home";
 import {Pagination} from '../components/Pagination/Pagination';
+import { Footer } from "../components/Footer/Footer";
 import {useState, useEffect} from 'react';
 import * as cryptosService from '../services/cryptosService';
 import './root.css';
@@ -12,15 +13,22 @@ export default function Root() {
   const ITEMS_PER_PAGE: number = 10; 
 
   const [cryptos, setCryptos] = useState<Crypto[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
 
   useEffect(() => {
     cryptosService.getAll().then((data: Crypto[]) => {
-      setCryptos(data);
+      const filteredCryptos = data.filter((crypto: Crypto) => 
+          crypto.name.toLowerCase().includes(searchTerm.toLowerCase()
+      ));
+
+    setCryptos(filteredCryptos);
+
     })
     .catch((error) => {
       console.log(error);
     })
-  },[])
+  },[searchTerm])
 
   
 
@@ -29,11 +37,28 @@ export default function Root() {
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
+
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    console.log(searchTerm);
+    console.log(value);
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    setCurrentPage(1);
+  }
   
     return(
-      <div>
+      <div className="main">
+        <div className="search-box">
+            <button className="btn-search"><i className="fa-search"></i></button>
+            <input type="text" className="input-search" placeholder="Type to Search..." onChange={handleSearch} onBlur={handleBlur} />
+        </div>
         <Home cryptos={cryptos} currentPage={currentPage} itemsPerPage={ITEMS_PER_PAGE}/>
         <Pagination totalPages={totalPages} currentPage={currentPage} onPageChange={handlePageChange} />
+        <Footer />
       </div>
     );
   }
